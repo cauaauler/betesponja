77a37e2af870db2018fdf0ac2b0142ee20661aa3
Cauã Auler <cauaauler@gmail.com>
2025-08-19 10:54:19 -0300

    removendo arquivos desnecessários

--- bet/obj/Debug/net9.0/FutebolSimplesBetsHub.AssemblyInfo.cs
+++ bet/obj/Debug/net9.0/FutebolSimplesBetsHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FutebolSimplesBetsHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb54ef2ca0034959238ebc09b701cb93088b08ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d95c93c70614c2887ad544e572444f12e3dd4d29")]
 [assembly: System.Reflection.AssemblyProductAttribute("FutebolSimplesBetsHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FutebolSimplesBetsHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
